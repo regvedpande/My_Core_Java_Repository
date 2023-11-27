a7a3d306944e0d97a11f93c1aace6d2ae8f14db8
regvedpande <149050058+regvedpande@users.noreply.github.com>
2023-11-27 14:54:17 +0530

    Using Object File

--- src/com/mkpits/datatype/UsingObjectFromAnotherClass.java
+++ src/com/mkpits/datatype/UsingObjectFromAnotherClass.java
@@ -3,7 +3,7 @@ package com.mkpits.datatype;
 public class UsingObjectFromAnotherClass {
 
 	public static void main(String[] args) {
-		MyClass mc = new MyClass();
-		mc.myStaticMethod();
+		MyClass cp = new MyClass();
+		MyClass.myStaticMethod();
 	}
 }
