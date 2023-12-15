e65e7403dd13bee9ccc3a16e98e8e62eb46d231c
regvedpande <149050058+regvedpande@users.noreply.github.com>
2023-12-15 17:11:52 +0530

    Changed some properties of my Loops Program

--- src/com/mkpits/loops/AnotherInnerLoop.java
+++ src/com/mkpits/loops/AnotherInnerLoop.java
@@ -6,25 +6,22 @@ import java.io.InputStreamReader;
 
 public class AnotherInnerLoop {
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
+    public static void main(String[] args) throws NumberFormatException, IOException {
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		System.out.println("Enter the Number");
-		int n = Integer.parseInt(br.readLine());
-		for(int i=1;i<=n;i++) 
-		{       
-			for(int k=1;k<=i;k++)
-			{
-				System.out.print("  ");
-			}
-			
-		    for(int j=n ;j>=i;j--)
-			{                   
-			System.out.print("   + "); 
-			}
-		    
-		    System.out.println();
-		
-		}
-	}
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter the Number");
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= n; i++) {
+            for (int k = 1; k <= i; k++) {
+                System.out.print("  ");
+            }
+
+            for (int j = n; j >= i; j--) {
+                System.out.print("   + ");
+            }
+
+            System.out.println();
+        }
+    }
 }
