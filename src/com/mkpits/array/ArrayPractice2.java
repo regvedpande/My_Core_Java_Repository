7ca14777b4e8ee2e608647b61d77589f67873850
regvedpande <149050058+regvedpande@users.noreply.github.com>
2023-12-19 16:54:45 +0530

    Sample Array Practice Codes

--- src/com/mkpits/array/ArrayPractice2.java
+++ src/com/mkpits/array/ArrayPractice2.java
@@ -2,9 +2,16 @@ package com.mkpits.array;
 
 public class ArrayPractice2 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
+        String[][] names = {
+            {"ayush",  "regved"},
+            {"sarvesh",  "sagar" }
+        };
 
-	}
-
+        for (int i = 0; i < names.length; ++i) {
+            for (int j = 0; j < names[i].length; ++j) {
+                System.out.println(names[i][j]); 
+            }
+        }
+    }
 }
