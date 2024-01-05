960a1dbff69286ed93b74eb623470cdaacbdf812
regvedpande <149050058+regvedpande@users.noreply.github.com>
2024-01-05 20:46:49 +0530

    Some Changes

---
+++ src/com/mkpits/constructor/CallPrivateConstructor.java
@@ -0,0 +1,11 @@
+package com.mkpits.constructor;
+
+public class CallPrivateConstructor {
+
+	public static void main(String[] args) {
+
+		ConstructorConcepts c = new ConstructorConcepts();
+
+	}
+
+}
---
+++ src/com/mkpits/constructor/ConstructorConcepts.java
@@ -0,0 +1,11 @@
+package com.mkpits.constructor;
+
+public class ConstructorConcepts {
+
+	int a;
+
+	private void ConstructorConcepts(int a) {
+		System.out.println("private ");
+
+	}
+}
