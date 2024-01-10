5fe63b382f50cd07cacad2f1f13a7c5cee27a845
regvedpande <149050058+regvedpande@users.noreply.github.com>
2024-01-10 22:33:58 +0530

    Reversing Array and som loops program practice

--- src/com/mkpits/bufferedreader/CallingBufferedReader.java
+++ src/com/mkpits/bufferedreader/CallingBufferedReader.java
@@ -4,8 +4,8 @@ public class CallingBufferedReader {
 
 	public static void main(String[] args) throws Exception {
 		
-        Registration registration = new Registration();
-		registration.register();
+//        Registration registration = new Registration();
+//		registration.register();
 		
 		System.out.println("First Name is: "+registration.firstName);
 		System.out.println("Last Name is: "+registration.lastName);
@@ -14,3 +14,5 @@ public class CallingBufferedReader {
 	}
 
 }
+
+
---
+++ src/com/mkpits/loops/Factorial.java
@@ -0,0 +1,20 @@
+package com.mkpits.loops;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int regvedage = 5;
+		int factorial = 1;
+		int i = 1;
+
+		int num = 10;
+		while (i <= num) {
+			factorial *= i;
+			i++;
+		}
+		System.out.println("Factorial of " + num + " is: " + factorial);
+	}
+
+}
