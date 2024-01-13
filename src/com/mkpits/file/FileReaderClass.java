32bbea1431bf2ad4357b2c1c5b8b409e45526231
regvedpande <149050058+regvedpande@users.noreply.github.com>
2024-01-13 22:33:40 +0530

    Classes and Constructor Concepts

--- src/com/mkpits/file/FileReaderClass.java
+++ src/com/mkpits/file/FileReaderClass.java
@@ -9,11 +9,12 @@ public class FileReaderClass {
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 
-		FileReader fr = new FileReader("D:\\AnotherFile.txt");
+		FileReader fr = new FileReader("D:\\mkpits_workspace\\MKPITS_CORE_JAVA_REGVED_PANDE\\src\\com\\mkpits\\file\\FileInputStreamReader.java");
 		BufferedReader br = new BufferedReader(fr);
 		String read = "";
+		// we are assigning words one by one in read which must be a String Type
 		while ((read = br.readLine()) != null) {
-			System.out.print(read);
+			System.out.println(read );
 
 		}
 
