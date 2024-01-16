9e9ef21938032e0b764fdde680958113fb2fb333
regvedpande <149050058+regvedpande@users.noreply.github.com>
2024-01-16 17:33:01 +0530

    Exception Handling

--- src/com/mkpits/file/FileReaderClass.java
+++ src/com/mkpits/file/FileReaderClass.java
@@ -9,12 +9,15 @@ public class FileReaderClass {
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 
-		FileReader fr = new FileReader("D:\\mkpits_workspace\\MKPITS_CORE_JAVA_REGVED_PANDE\\src\\com\\mkpits\\file\\FileInputStreamReader.java");
+		// If i edit the file argument adding or renaming it it may show
+		// FileNotFoundException
+		FileReader fr = new FileReader(
+				"D:\\mkpits_workspace\\MKPITS_CORE_JAVA_REGVED_PANDE\\src\\com\\mkpits\\file\\FileInputStreamReader.java");
 		BufferedReader br = new BufferedReader(fr);
 		String read = "";
 		// we are assigning words one by one in read which must be a String Type
 		while ((read = br.readLine()) != null) {
-			System.out.println(read );
+			System.out.println(read);
 
 		}
 
