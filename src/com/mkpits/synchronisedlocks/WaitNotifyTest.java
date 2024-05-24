b54fab592640958595a1d7a8e3590e419fbfa39a
regvedpande <149050058+regvedpande@users.noreply.github.com>
2024-05-24 23:36:05 +0530

    Some Work

--- src/com/mkpits/synchronisedlocks/WaitNotifyTest.java
+++ src/com/mkpits/synchronisedlocks/WaitNotifyTest.java
@@ -8,7 +8,7 @@ public class WaitNotifyTest {
     public static void main(String[] args) {
         Message msg = new Message("process it");
         Waiter waiter1 = new Waiter(msg);
-        new Thread(waiter, "waiter 1").start(); 
+        new Thread(waiter1, "waiter 1").start(); 
 
         Waiter waiter2 = new Waiter(msg);
         new Thread(waiter2, "waiter 2").start(); 
